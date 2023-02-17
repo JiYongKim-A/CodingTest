493929a6c38a0d86c65b94bf9e38d262077e28ca
JiYongKim <colorful8315@gmail.com>
2023-02-17 17:15:17 +0900

    docs(athlete.java) : add time complexity

--- Programmers/Hash/athlete_who_didn't_finish/athlete.java
+++ Programmers/Hash/athlete_who_didn't_finish/athlete.java
@@ -1,5 +1,6 @@
 import java.util.*;
 class Solution {
+    // 시간 복잡도 O(n) ... n = participant.length - 1 
     public String solution(String[] participant, String[] completion) {
         Arrays.sort(participant);
         Arrays.sort(completion);
