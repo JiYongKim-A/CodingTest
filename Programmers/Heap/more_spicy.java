28ba5c4d184186b5ca9907bb17feece91130cd3c
JiYongKim <colorful8315@gmail.com>
2023-03-15 18:45:16 +0900

    feat (more_spicy.java) : Programmers more_spicy ploblem solving method

---
+++ Programmers/Heap/more_spicy.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i : scoville) {
+            queue.add(i);
+        }
+
+        int count = 0;
+        while (queue.peek() < K) {
+            if (queue.size() > 1) {
+                count++;
+                int a = queue.poll();
+                int b = queue.poll();
+                queue.add(a + b * 2);
+            } else {
+                return -1;
+            }
+        }
+        return count;
+    }
+}
