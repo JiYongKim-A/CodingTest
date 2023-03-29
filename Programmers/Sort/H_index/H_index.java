547d4086905aa47a70b614f9a21d2a1ead608baa
unknown <colorful8315@gmail.com>
2023-03-29 15:19:34 +0900

    feat(H_index.java) : Programmers H_Index ploblem solving method

---
+++ Programmers/Sort/H_index/H_index.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        Integer[] arr = Arrays.stream(citations).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr,Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++) {
+            if (i+1 > arr[i]) {
+                return i;
+            }
+        }
+        return citations.length;
+    
+    }
+}
