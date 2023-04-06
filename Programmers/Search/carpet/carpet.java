3c1a2eeb15f8800372daef88fbc3c8a7c6834666
JiYongKim <colorful8315@gmail.com>
2023-04-06 19:09:02 +0900

    feat (carpet.java) : Programmers carpet ploblem solving method

---
+++ Programmers/Search/carpet/carpet.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int brown, int yellow) { List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= yellow; i++) {
+            if (yellow % i == 0) {
+                list.add(i);
+            }
+        }
+        int[] result = new int[2];
+        for (int i : list) {
+            int width = Math.max(i, yellow / i);
+            int height = Math.min(i, yellow / i);
+            if (width * 2 + height * 2 + 4 == brown) {
+                result[0] = width + 2;
+                result[1] = height + 2;
+            }
+            
+        }
+        
+        return result;
+    }
+}
