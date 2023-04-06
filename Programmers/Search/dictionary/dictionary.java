d61a1eb28e03d042db333e25cba9e1ff934b6c22
JiYongKim <colorful8315@gmail.com>
2023-04-06 19:09:33 +0900

    feat (dictionary.java) : Programmers dictionary ploblem solving method

---
+++ Programmers/Search/dictionary/dictionary.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(String word) {
+       HashMap<String, Integer> alphabet = new HashMap<>();
+        HashMap<Integer, Integer> index = new HashMap<>();
+        alphabet.put("A", 0);
+        alphabet.put("E", 1);
+        alphabet.put("I", 2);
+        alphabet.put("O", 3);
+        alphabet.put("U", 4);
+        index.put(0, 781);
+        index.put(1, 156);
+        index.put(2, 31);
+        index.put(3, 6);
+        index.put(4, 1);
+
+        String[] split = word.split("");
+        int result = 0;
+        for (int i = 0; i < split.length; i++) {
+             result+=alphabet.get(split[i]) * index.get(i) + 1;
+        }
+        return result;
+    }
+}
